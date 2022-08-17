c38fea775368a1340741ab8d0b1b4bf0da06dd5d
t1m <azi42@yahoo.com>
2022-08-18 03:43:54 +0800

    reduce further

--- .eleventy.js
+++ .eleventy.js
@@ -1,22 +1,12 @@
 module.exports = function (eleventyConfig) {
-	
-	eleventyConfig.addPassthroughCopy("./src/css/");
-	eleventyConfig.addWatchTarget("./src/css/");
-	
-	eleventyConfig.addPassthroughCopy("./src/js/");
-	eleventyConfig.addWatchTarget("./src/js/");
-	
-	eleventyConfig.addPassthroughCopy("./src/images/");
-	eleventyConfig.addWatchTarget("./src/images/");
-	
-	eleventyConfig.addPassthroughCopy("./src/contracts/");
-	eleventyConfig.addWatchTarget("./src/contracts/");
-	
-	return {
-		dir: {
-			input: "src",
-			output:"public",
-		},
-	};
-};
-	
+  eleventyConfig.addPassthroughCopy('./src/js/')
+  eleventyConfig.addWatchTarget('./src/js/')
+  eleventyConfig.addPassthroughCopy('./src/contracts/')
+  eleventyConfig.addWatchTarget('./src/contracts/')
+  return {
+    dir: {
+      input: 'src',
+      output: 'public',
+    },
+  }
+}
